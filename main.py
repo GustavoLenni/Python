824909ef5a2274b5b75dd13b49788e9e7351c181
GustavoLenni <133364786+GustavoLenni@users.noreply.github.com>
2025-03-31 13:35:47 -0300

    new-commit-2

--- main.py
+++ main.py
@@ -286,10 +286,3 @@
 
 # print("Tivemos o total de: ", len(vendedores), "Vendedores")
 
-
-class ControleRemoto:
-    def __init__(self,cor,altura,profundidade,largura):
-        self.cor = cor
-        self.altura = altura
-        self.profundidade = profundidade
-        self.largura = largura
